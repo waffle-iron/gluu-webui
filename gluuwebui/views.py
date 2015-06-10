6e7bc3e1aefaa0da29529cf9edc5a22286190183
tecoholic <arun@arunmozhi.in>
2015-06-10 06:53:52 +0530

    added some base html files and views

--- gluuwebui/views.py
+++ gluuwebui/views.py
@@ -1,5 +1,21 @@
 from gluuwebui import app
+from flask import render_template
 
 @app.route("/")
-def hello():
-    return "hello"
+def index():
+    return render_template( "index.html" )
+
+@app.route("/node")
+def list_nodes():
+    # TODO call the API and get the list
+    return render_template( "entity_list.html", name="Nodes" )
+
+@app.route("/provider")
+def list_providers():
+    # TODO call the API and get the list
+    return render_template( "entity_list.html", name="Providers" )
+
+@app.route("/cluster")
+def list_clusters():
+    # TODO call the API and get the list
+    return render_template( "entity_list.html", name="Clusters" )
