afdc15aa38aa0547271fac4e2ff58d57342aa461
tecoholic <arun@arunmozhi.in>
2015-06-24 12:37:59 +0530

    added tests for url responses of get

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,24 +1,40 @@
-from nose.tools import *
+from nose.tools import assert_equal, with_setup
+from mock import MagicMock
 
 import gluuwebui
-
+import requests
 
 gluuwebui.app.config['TESTING'] = True
 app = gluuwebui.app.test_client()
 
+resources = ['/provider', '/cluster', '/node', '/license',
+             '/license_credential']
 
+
+def setup_empty_get():
+    requests.get = MagicMock()
+    requests.get.return_value.status_code = 200
+    requests.get.return_value.json.return_value = {}
+
+
+@with_setup(setup_empty_get)
 def test_index():
     res = app.get('/')
     assert_equal(200, res.status_code)
 
 
+def check_status(item):
+    res = app.get(item)
+    assert_equal(200, res.status_code)
+
+
+@with_setup(setup_empty_get)
 def test_resources_get():
-    resources = ['/provider', '/cluster', '/node', '/license',
-                 '/license_credential']
-
-    def check_status(item):
-        res = app.get(item)
-        assert_equal(200, res.status_code)
-
     for item in resources:
         yield check_status, item
+
+
+@with_setup(setup_empty_get)
+def test_resource_with_id_get():
+    for item in resources:
+        yield check_status, item+"/some_id"
