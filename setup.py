8e7294fda6399b7cc9c75b2f61a5be5834fb963e
Isman Firmansyah <isman.firmansyah@gmail.com>
2015-07-07 00:21:39 +0700

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
         name="Gluu-WebUI",
-        version="0.1.0",
+        version="0.1.1",
         url="https://github.com/GluuFederation/gluu-webui",
         license="MIT",
         author="Gluu",
