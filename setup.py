77c3708a6d39c06b752ae1a72b9d3e2f6042c9b1
Arunmozhi <arun@arunmozhi.in>
2015-11-17 18:28:27 +0530

    updated setup.py with version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
         name="Gluu-WebUI",
-        version="0.1.2",
+        version="0.3",
         url="https://github.com/GluuFederation/gluu-webui",
         license="MIT",
         author="Gluu",
